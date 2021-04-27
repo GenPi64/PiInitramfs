1963a90549af0295b85ba2abb69962cddb08f7f7
Logan Perkins <logan@git.lp-programming.com>
2021-04-27 16:08:53 -0700

    Implement simple popen function. Lookup root device via blkid

---
+++ lib/python/popen.py
@@ -0,0 +1,26 @@
+import os
+
+class Popen:
+    def __init__(self, cmd, stdin=None, stdout=None, stderr=None):
+        if stdin is None:
+            stdin, self.stdin = os.pipe()
+        if stdout is None:
+            self.stdout, stdout = os.pipe()
+        if stderr is None:
+            self.stderr, stderr = os.pipe()
+            
+        self.child = os.fork()
+        if self.child:
+            return
+        os.close(0)
+        os.close(1)
+        os.close(2)
+        os.dup(stdin)
+        os.dup(stdout)
+        os.dup(stderr)
+        os.execvp(cmd[0], cmd)
+    def wait(self):
+        r = os.waitpid(self.child, 0)
+        return r[1]
+        
+        
